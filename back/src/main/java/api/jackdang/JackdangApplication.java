35a91a7c156a30482d6d940e89037ab643a2c776
sang-rak <sangrack114@naver.com>
2023-08-02 23:15:27 +0900

    [BE/Sg-0] feat: 프로젝트 생성

---
+++ back/src/main/java/api/jackdang/JackdangApplication.java
@@ -0,0 +1,13 @@
+package api.jackdang;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JackdangApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JackdangApplication.class, args);
+	}
+
+}
---
+++ back/src/test/java/api/jackdang/JackdangApplicationTests.java
@@ -0,0 +1,13 @@
+package api.jackdang;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JackdangApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
