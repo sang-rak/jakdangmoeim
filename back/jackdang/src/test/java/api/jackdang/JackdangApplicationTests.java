33ebc15be19556ab75d539d338b334c3e40e6b13
sang-rak <sangrack114@naver.com>
2023-08-06 23:10:28 +0900

    [BE/Sg-0] feat: Querydsl 검증

--- back/jackdang/src/test/java/api/jackdang/JackdangApplicationTests.java
+++ back/jackdang/src/test/java/api/jackdang/JackdangApplicationTests.java
@@ -1,13 +1,40 @@
 package api.jackdang;
 
+
+import api.jackdang.entity.Hello;
+import api.jackdang.entity.QHello;
+import com.querydsl.jpa.impl.JPAQueryFactory;
+import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.transaction.annotation.Transactional;
+
+import javax.persistence.EntityManager;
+
+import static org.assertj.core.api.Assertions.*;
 
 @SpringBootTest
+@Transactional
 class JackdangApplicationTests {
+
+	@Autowired
+	EntityManager em;
 
 	@Test
 	void contextLoads() {
+		Hello hello = new Hello();
+		em.persist(hello);
+
+		JPAQueryFactory query = new JPAQueryFactory(em);
+		QHello qHello = QHello.hello;
+
+		Hello result = (Hello) query
+				.selectFrom(qHello)
+				.fetchOne();
+
+		assertThat(result).isEqualTo(hello);
+		assertThat(result.getId()).isEqualTo(hello.getId());
 	}
 
 }
