3b3f1e754f0b9b05a2bd201b8a53ecbce842d30b
sang-rak <sangrack114@naver.com>
2023-08-22 22:14:40 +0900

    [BE/Sg-0] fix: querydsl 커스텀버그 수정  #12

--- back/jackdang/src/main/java/api/jackdang/repository/MemberTestRepository.java
+++ back/jackdang/src/main/java/api/jackdang/repository/MemberTestRepository.java
@@ -9,7 +9,7 @@ import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.support.PageableExecutionUtils;
 import org.springframework.stereotype.Repository;
-
+import static org.springframework.util.StringUtils.hasText;
 import java.util.List;
 
 import static api.jackdang.entity.QMember.member;
