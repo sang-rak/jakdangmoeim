cd90a048b08e4403bb8db6b9fefd58875ef262b4
sang-rak <sangrack114@naver.com>
2023-10-25 19:58:24 +0900

    [FE/Sg-1] feat: 기본 페이지접근시  login페이지로 리다이렉션 기능 #1

--- front/pages/_app.tsx
+++ front/pages/_app.tsx
@@ -1,10 +1,16 @@
-import React from "react";
+import React, { useEffect } from "react";
 import type { AppProps } from "next/app";
 import Head from "next/head";
 
 import wrapper from "../store/configureStore";
+import { useRouter } from "next/router";
 
 const jackdong = ({ Component, pageProps }: AppProps) => {
+  const router = useRouter();
+
+  useEffect(() => {
+    router.push("/login"); // / 페이지로 오면 /login 페이지로 리다이렉션
+  }, []);
   return (
     <>
       {/* 공통 레이아웃 */}
