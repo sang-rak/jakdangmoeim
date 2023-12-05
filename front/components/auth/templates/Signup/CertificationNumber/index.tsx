de3cdee900f987995f5a92ec13023b679cdd3cc2
sang-rak <sangrack114@naver.com>
2023-12-05 23:23:54 +0900

    [FE/Sg-1-3] feat: 번호인증#1

--- front/components/auth/templates/Signup/CertificationNumber/index.tsx
+++ front/components/auth/templates/Signup/CertificationNumber/index.tsx
@@ -7,14 +7,17 @@ import AppLayout from "../../../../common/organisms/AppLatout";
 import Title from "../../../../common/atoms/Title";
 import { ErrorMessage, FlexWrapper, LinkWrapper } from "./styles";
 import { ButtonWrapper } from "./styles";
+import { ExclamationCircleFilled } from "@ant-design/icons";
+import Lavel from "../../../../common/atoms/Lavel";
 
 const CertificationNumber = () => {
   const dispatch = useDispatch();
 
   const [certificationnumber, setCertificationNumber] = useState(false);
-
+  const [certificationnumberError, setCertificationnumberError] =
+    useState(false);
   const onSubmit = useCallback(() => {}, []);
-
+  // "/auth/signup/passwordinfo"
   return (
     <AppLayout>
       <FlexWrapper gap="large" vertical>
@@ -23,30 +26,40 @@ const CertificationNumber = () => {
           <Title content="가입하기" customStyle={{ margin: 0 }} />
         </div>
         <Form onFinish={onSubmit} layout="vertical">
-          <Form.Item>
-            <Form.Item
-              label="회원님의 휴대폰으로 전송된  
-            인증번호를 입력해주세요"
-            ></Form.Item>
-          </Form.Item>
+          <Form>
+            <Lavel
+              content="회원님의 휴대폰으로 전송된 "
+              customStyle={{ margin: 0 }}
+            ></Lavel>
+            <Lavel
+              content="인증번호를 입력해주세요"
+              customStyle={{ margin: 0 }}
+            ></Lavel>
+          </Form>
+
           <Form.Item>
             <br />
             <Input
               name="user-certificationnumber-check"
               type="certificationnumber"
               placeholder="인증번호 6자리"
+              suffix={"02:57"}
               required
             />
-
-            <ErrorMessage>인증번호를 다시 확인 해주세요</ErrorMessage>
+            <Form.Item>
+              {certificationnumber && (
+                <ErrorMessage>
+                  <ExclamationCircleFilled />
+                  &ensp;인증번호를 다시 확인 해주세요
+                </ErrorMessage>
+              )}
+            </Form.Item>
           </Form.Item>
 
           <FlexWrapper>
-            <LinkWrapper href="/auth/signup/passwordinfo">
-              <ButtonWrapper type="primary" htmlType="submit" block>
-                다음
-              </ButtonWrapper>
-            </LinkWrapper>
+            <ButtonWrapper type="primary" htmlType="submit" block>
+              다음
+            </ButtonWrapper>
           </FlexWrapper>
         </Form>
       </FlexWrapper>
---
+++ front/components/common/atoms/Lavel/styles.ts
@@ -0,0 +1,5 @@
+import { Form } from "antd";
+import styled from "styled-components";
+
+// 직접 스타일 추가
+export const StyledLavel = styled(Form.Item)``;
