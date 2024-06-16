f923977c964b9da662960d71c5ba905761ad3deb
IngRak <62869880+sang-rak@users.noreply.github.com>
2024-06-16 20:50:39 +0900

    Feature/로그인페이지/#4/비밀번호찾기 (#42)
    * [Lo-2-1] feat: 패스워드 찾기 링크설정 및 페이지 생성
    
    * [Lo-2-1] feat: 패스워드 찾기 페이지 표지 개발

--- front/components/auth/templates/Login/FindPassword/index.tsx
+++ front/components/auth/templates/Login/FindPassword/index.tsx
@@ -1,10 +1,49 @@
-import React, { memo } from "react";
+import React, { memo, useCallback, useEffect, useState } from "react";
 import AppLayout from "../../../../common/organisms/AppLatout";
+import { LinkWrapper, FlexWrapper, FormWrapper } from "./styles";
+import { ArrowLeftOutlined } from "@ant-design/icons";
+import { Flex, Form, Input } from "antd";
+import Title from "../../../../common/atoms/Title";
+import { ButtonWrapper } from "../styles";
+import useInput from "../../../../../hooks/useInput";
 
 const FindPassword = () => {
+  const [phone, onChangePhone] = useInput("");
+
+  const onSubmit = useCallback(() => {
+    console.log("test");
+  }, [phone]);
+
   return (
     <AppLayout>
-      <div>패스워드찾기</div>
+      <LinkWrapper href="/auth/login">
+        <ArrowLeftOutlined />
+      </LinkWrapper>
+      <FlexWrapper gap={100} justify="center" vertical>
+        <Flex align="left" vertical>
+          <Title content="비밀번호를" customStyle={{ margin: 0 }} />
+          <Title content="잊으셨나요?" customStyle={{ margin: 0 }} />
+        </Flex>
+        <FormWrapper onFinish={onSubmit} layout="vertical">
+          <Form.Item>
+            <label htmlFor="user-phone">전화번호를 입력해주세요</label>
+          </Form.Item>
+          <Form.Item>
+            <Input
+              name="user-phone"
+              type="phone"
+              value={phone}
+              onChange={onChangePhone}
+              required
+              placeholder="전화번호"
+            />
+          </Form.Item>
+
+          <ButtonWrapper type="primary" htmlType="submit" block>
+            인증번호 받기
+          </ButtonWrapper>
+        </FormWrapper>
+      </FlexWrapper>
     </AppLayout>
   );
 };
